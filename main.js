777373283b41af1e360759e359a63e8b10f67b2c
Vincent Nien <48661745+vincentnienx@users.noreply.github.com>
2019-04-11 15:48:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,21 +8,21 @@ var server = http.createServer(function (req, res) {   //create web server
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
         // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.write('<html><body><p>This is !NEW! home Page.</p></body></html>');
         res.end();
     
     }
     else if (req.url == "/student") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.write('<html><body><p>This is !NEW! student Page.</p></body></html>');
         res.end();
     
     }
     else if (req.url == "/admin") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.write('<html><body><p>This is !NEW! admin Page.</p></body></html>');
         res.end();
     
     }
