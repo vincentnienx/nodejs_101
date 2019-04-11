5fb9bf221b2a218e459b8e1f647ef6d698014b4f
Vincent Nien <48661745+vincentnienx@users.noreply.github.com>
2019-04-11 14:29:55 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http'); // Import Node.js core module
 
 var server = http.createServer(function (req, res) {   //create web server
+    console.log(req.url)
     if (req.url == '/') { //check the URL of the current request
         
         // set response header
@@ -30,6 +31,7 @@ var server = http.createServer(function (req, res) {   //create web server
 
 });
 
-server.listen(5000); //6 - listen for any incoming requests
+var port = process.env.PORT || 8092;
+server.listen(port); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log(`Node.js web server at port ${port} is running..`)
